ebba0697f81e0b77a2b0f7df9ede27c13bd89cfe
Student_MA102 <aidarous12@gmail.com>
2020-07-25 17:20:30 -0400

    add logo to home screen

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import logo from '../assets/logo.jpg'
 
 class HomePage  extends Component {
     render() {
@@ -11,7 +12,9 @@ class HomePage  extends Component {
                    <Link to="/user-profile"> User Profile </Link>
                    <Link to="/bookmarks"> Bookmarks </Link>
                 </nav>
-                <h1> Photo App</h1>
+                <h1> React Photo App</h1>
+                <img src= {logo} />
+                
                 
             </div>
         </Router>
