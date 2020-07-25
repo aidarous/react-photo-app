a7df10d474c102fe96c5e235105af76741b36412
Student_MA102 <aidarous12@gmail.com>
2020-07-25 15:50:37 -0400

    Added heading

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -8,8 +8,10 @@ class HomePage  extends Component {
             <div>
                 <nav>
                    <Link to="/home"> Home </Link>
-                   <Link to="/home"> User Profile </Link>
+                   <Link to="/user-profile"> User Profile </Link>
+                   <Link to="/bookmarks"> Bookmarks </Link>
                 </nav>
+                <h1> Photo App</h1>
                 
             </div>
         </Router>
