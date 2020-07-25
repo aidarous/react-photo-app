1a2ddf80caa522bd0462a2670c8eaa5e701f538d
Student_MA102 <aidarous12@gmail.com>
2020-07-24 23:24:26 -0400

    added router tag

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,9 +3,12 @@ import React, { Component } from 'react'
 class HomePage  extends Component {
     render() {
         return (
+        <Router>
             <div>
                 
             </div>
+        </Router>
+            
         )
     }
 }
