9d6efdfa69db9a9da2f33138fa718060486526ec
Student_MA102 <aidarous12@gmail.com>
2020-07-26 12:21:03 -0400

    changed image sizes

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -16,8 +16,8 @@ class HomePage  extends Component {
                 </div>
 
                 <h1> React Photo App</h1>
-                <img src= {logo} className= "img-fluid rounded" alt= "this" /><br />
-                <img src= {somethinggreat} className= "img-fluid" alt= "this" />
+                <img src= {logo} className= "img-thumbnail" alt= "this" /><br />
+                <img src= {somethinggreat} className= "img-thumbnail" alt= "this" />
                 
                 
             </div>
