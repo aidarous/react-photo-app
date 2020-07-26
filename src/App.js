3592b929556b193c4357f25b1c29f02ee4c7814d
Student_MA102 <aidarous12@gmail.com>
2020-07-26 14:31:32 -0400

    imported UserProfile.js to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import HomePage from './components/HomePage'
+import UserProfile from './components/UserProfile'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <HomePage />
+      
     </div>
   );
 }
