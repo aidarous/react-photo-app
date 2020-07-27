93f608c082b9ce31a25b8dd8d63690b38ad23343
Student_MA102 <aidarous12@gmail.com>
2020-07-27 00:05:44 -0400

    created dislike button

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -11,7 +11,7 @@ class UserProfile extends Component {
                 <button className="btn btn-primary"><span className= "fas fa-thumbs-up"></span> Like</button>
                 </div>
                 <div>
-                    <button className=" btn btn-danger"></button>
+                    <button className=" btn btn-danger">Dislike</button>
                 </div>
                 
             </div>
