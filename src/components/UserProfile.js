7efd8c05481a82de2bd3190623d00975be265e5e
aidarous <aidarous12@gmail.com>
2020-07-27 23:57:32 -0400

    input type test

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -32,7 +32,7 @@ class UserProfile extends Component {
                     <div className="col-xs-4">
                     <button className="btn btn-primary">Like</button>
                     <button className=" btn btn-danger">Dislike</button><br />
-                    <input></input>
+                    <input type="text"></input>
                     </div>
                 </div>
             </div>
