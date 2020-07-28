9b709e950066a47ea21085997697b469e8825d63
aidarous <aidarous12@gmail.com>
2020-07-27 23:23:28 -0400

    rearranged layout

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,14 +7,15 @@ class HomePage  extends Component {
     render() {
         return (
             <div>
-                <h1> React Photo App</h1>
-                <img src= {logo} className= "img-thumbnail" alt= "this" /><br />
-                <img src= {somethinggreat} className= "img-thumbnail" alt= "this" />
                 <form className="ml-auto">
                         <input type="text" placeholder="Username" />
                         <input type="password" placeholder="Password" />
                         <button type="submit" className="btn btn-default">Login</button>
                 </form>
+                <h1> React Photo App</h1>
+                <img src= {logo} className= "img-thumbnail" alt= "this" /><br />
+                <img src= {somethinggreat} className= "img-thumbnail" alt= "this" />
+                
                 
                 
             </div>
