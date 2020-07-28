c00602317eae3d73480beba24e834b6b8e2b3e9d
aidarous <aidarous12@gmail.com>
2020-07-27 23:15:35 -0400

    rearranged layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import HomePage from './components/HomePage'
-
+import UserProfile from './components/UserProfile'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <HomePage />
+      
+      <div className="row">
+                    <nav className="navbar navbar-expand-md bg-light">
+                    <Link to="/HomePage"> Home </Link>
+                    <Link to="/UserProfile"> User Profile</Link>
+                    <Link to="/bookmarks"> Bookmarks</Link>
+                    </nav>
+      </div>     
+      <Switch>
+        <Route path='/HomePage'>
+          <HomePage />
+        </Route>
+        <Route path="/UserProfile">
+            <UserProfile />
+          </Route>
+
+            </Switch>
+         
+
+      
+      
       
     </div>
+    </Router>
   );
 }
 
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,35 +1,12 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+
 import logo from '../assets/logo.jpg'
 import somethinggreat from '../assets/something-great.jpg'
-import UserProfile from './UserProfile'
+
 class HomePage  extends Component {
     render() {
         return (
-        <Router>
             <div>
-                <div className="row">
-                    <nav className="navbar navbar-expand-md bg-light">
-                    <Link to="/home"> Home </Link>
-                    <Link to="/user-profile"> User Profile</Link>
-                    <Link to="/bookmarks"> Bookmarks</Link>
-                    </nav>
-                    <form className="ml-auto">
-                        <input type="text" placeholder="Username" />
-                        <input type="password" placeholder="Password" />
-                        <button type="submit" className="btn btn-default">Login</button>
-                    </form>
-                </div>
-                {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-                <Switch>
-                    
-                    <Route path="/user-profile">
-                        <UserProfile />
-                    </Route>
-
-                </Switch>
-
                 <h1> React Photo App</h1>
                 <img src= {logo} className= "img-thumbnail" alt= "this" /><br />
                 <img src= {somethinggreat} className= "img-thumbnail" alt= "this" />
@@ -37,7 +14,7 @@ class HomePage  extends Component {
                 
                 
             </div>
-        </Router>
+        
             
         );
     }
