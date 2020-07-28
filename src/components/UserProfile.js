6842e5d3993839eab4a170653bd2a38d0351d2fe
aidarous <aidarous12@gmail.com>
2020-07-27 23:51:47 -0400

    imported photo

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import likes from '../assets/likes.jpg'
 import camera from '../assets/camera.jpg'
-
+import thinkoutside from '../assets/think-outside.jpg'
 class UserProfile extends Component {
     render() {
         return (
             <div className="col-md">
-                <h1>Profile</h1>
+                <h1>User Profile </h1>
                 <img src= {likes} className="img-rounded" alt="this"/><br/>
                 <div>
                     <div className="col-xs-4">
@@ -17,6 +17,7 @@ class UserProfile extends Component {
                         <input type="text" placeholder="Leave a comment"></input>
                         <button>post</button>
                     </div>
+
                     <img src={camera} alt="this" />
                     <div className="col-xs-4">
                     <button className="btn btn-primary">Like</button>
@@ -27,6 +28,7 @@ class UserProfile extends Component {
                     
                         <button>post</button>
                     </div>
+                    
                 </div>
             </div>
         )
