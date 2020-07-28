df274de9db353b5df6de06e3dfa2835dd7ede2d9
aidarous <aidarous12@gmail.com>
2020-07-27 23:54:57 -0400

    turned it into like button

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -30,7 +30,7 @@ class UserProfile extends Component {
                     </div>
                     <img src={thinkoutside} alt="this" />
                     <div className="col-xs-4">
-                    <button className="btn btn-primary"></button>
+                    <button className="btn btn-primary">Like</button>
                     </div>
                 </div>
             </div>
